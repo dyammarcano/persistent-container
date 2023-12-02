c86701ec3a101ae5d74bd9df716a1789b6a69ab3
Dyam Marcano <dyam.marcano@gmail.com>
2023-12-02 14:50:08 -0300

    Remove obsolete blob functionality from storage library
    The `myblob()` function from `blob.go` has been deleted in this commit. This function was previously responsible for creating a new blob client for Azure's Blob Storage service. However, with recent changes and refactoring of the storage module, this code is no longer necessary. It was removed to declutter the codebase and to ensure that all storage transactions use the updated and streamlined interfaces.

--- blob.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
-
-func myblob() {
-	clienr, err := azblob.NewBlobClient("https://mystorageaccount.blob.core.windows.net", azblob.NewAnonymousCredential(), nil)
-}
