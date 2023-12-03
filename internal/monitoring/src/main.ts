a2df87ba6c44df5e59e33c603bb6bbfac5084646
Dyam Marcano <dyam.marcano@gmail.com>
2023-12-02 22:20:56 -0300

    Remove redundant code from monitoring service
    This commit cleans up the codebase by removing redundant files from the internal monitoring service. These files were part of an initial template but are no longer needed as the service has evolved. Removing them reduces clutter and makes the project more maintainable.

--- internal/monitoring/src/main.ts
+++ internal/monitoring/src/main.ts
@@ -1,14 +1,5 @@
-import './main.css'
-
-import {createApp} from 'vue'
-import {createPinia} from 'pinia'
-
+import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import router from './router'
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount('#app')
